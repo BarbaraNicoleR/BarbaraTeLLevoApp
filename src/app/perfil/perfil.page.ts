d997942de70ebef8ffab343695fa24eb8857d689
BarbaraNicoleR <bar.riffo@duocuc.cl>
2023-11-30 13:20:08 -0300

    intento de listrar dartos del ususaeio

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -12,9 +12,27 @@ export class PerfilPage implements OnInit {
 
   constructor(private router:Router, public utilsSvc: UtilsService ) { }
 
-  
-
   ngOnInit() {
+    this.waitForUserData();
   }
-  
+
+  async waitForUserData() {
+    // Lógica para esperar a que los datos del usuario estén disponibles
+    // Esto podría ser una llamada a una función asincrónica que obtiene los datos del usuario
+    try {
+      // Lógica para obtener los datos del usuario
+      const userData = await this.utilsSvc.getUserData(); // Asegúrate de tener esta función en UtilsService
+
+      // Guardar datos en el LocalStorage
+      this.utilsSvc.saveInLocalStorage('name', userData.name);
+      this.utilsSvc.saveInLocalStorage('email', userData.email);
+      this.utilsSvc.saveInLocalStorage('rol', userData.rol);
+
+      // Imprimir datos en la consola para verificar
+      console.log('Datos del usuario:', userData.name, userData.email, userData.rol);
+    } catch (error) {
+      console.error('Error al obtener datos del usuario:', error);
+    }
+  }
 }
+  
--- src/app/services/utils.service.ts
+++ src/app/services/utils.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { LoadingController, ToastController, ToastOptions } from '@ionic/angular';
 import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,44 +11,39 @@ export class UtilsService {
     private loadingCtrl: LoadingController,
     private toastCtrl: ToastController,
     private router: Router
-  ) {}
+  ) { }
 
-
-  //===================loading================
+  // ===================loading================
   async loading() {
     const loading = await this.loadingCtrl.create({ spinner: 'crescent' });
     await loading.present();
     return loading;
   }
 
-  //===================Toast=================
+  // ===================Toast=================
   async presentToast(opts?: ToastOptions) {
-
     const toast = await this.toastCtrl.create(opts);
     toast.present();
   }
 
-  //=========== enruta cualquier pagina disponible==========
-
+  // =========== enruta cualquier pagina disponible==========
   routerLink(url: string) {
     return this.router.navigateByUrl(url);
   }
 
-
-  //=============0guarda elementos en localstorage=============00
-
+  // ============= guarda elementos en localstorage=============
   saveInLocalStorage(key: string, value: any) {
-    return localStorage.setItem(key, JSON.stringify(value));
+    console.log(`Guardando en localStorage: ${key}`, value);
+    localStorage.setItem(key, JSON.stringify(value));
   }
 
-  //========obtener elementos del localstorage=====
+  // ======== obtener elementos del localstorage =====
   getFromLocalStorage(key: string) {
-    return JSON.parse(localStorage.getItem(key))
-
+    return JSON.parse(localStorage.getItem(key));
   }
 
-  
-  
-
-
+  async getUserData() {
+    // Lógica para obtener los datos del usuario del localStorage
+    return this.getFromLocalStorage('user');
+  }
 }
